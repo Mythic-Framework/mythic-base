dae10bf9a84e578cdc1bff01fe4f8fda1e8d2147
BaskinB <54458253+BaskinB@users.noreply.github.com>
2026-03-05 13:38:58 -0600

    refactor: added checks for repo url
    This commit adds a check for the script's repo link for the update log.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,33 +3,34 @@ games { 'gta5' }
 lua54 'yes'
 
 version '1.0.0'
+repository 'https://github.com/Mythic-Framework/mythic-base'
 client_script "@mythic-pwnzor/client/check.lua"
 client_scripts {
-    'sh_init.lua',
-    'cl_init.lua',
-    'core/sh_*.lua',
-    'core/cl_*.lua',
-    'cl_config.lua',
-    'components/cl_*.lua',
+  'sh_init.lua',
+  'cl_init.lua',
+  'core/sh_*.lua',
+  'core/cl_*.lua',
+  'cl_config.lua',
+  'components/cl_*.lua',
 }
 
 server_script("@oxmysql/lib/MySQL.lua")
 server_scripts {
-    'sh_init.lua',
-    'sv_init.lua',
-    'sv_config.lua',
-    'core/sv_database.js',
-    'core/sv_generator.js',
-    'core/sv_regex.js',
-    'core/sh_*.lua',
-    'core/sv_*.lua',
-    'components/sv_*.lua',
+  'sh_init.lua',
+  'sv_init.lua',
+  'sv_config.lua',
+  'core/sv_database.js',
+  'core/sv_generator.js',
+  'core/sv_regex.js',
+  'core/sh_*.lua',
+  'core/sv_*.lua',
+  'components/sv_*.lua',
 }
 
 files {
-    'weaponanimations.meta',
-    'gta5.meta',
-    'water.xml',
+  'weaponanimations.meta',
+  'gta5.meta',
+  'water.xml',
 }
 
 replace_level_meta 'gta5'
