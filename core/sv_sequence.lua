2f43d553dfd196d732606a91b6353b069a7586a6
Artmines <arturowells@gmail.com>
2026-04-22 03:17:44 -0230

    feat(sequence changes): sequance changes for mysql

--- core/sv_sequence.lua
+++ core/sv_sequence.lua
@@ -1,5 +1,4 @@
 local _cachedSeq = {}
-local _loading = {}
 local _migrationComplete = false
 
 local function MigrateFromMongoDB()
@@ -9,13 +8,13 @@ end
 COMPONENTS.Sequence = {
     Get = function(self, key)
         if _cachedSeq[key] ~= nil then
-            _cachedSeq[key].sequence = _cachedSeq[key].sequence + 1
+            _cachedSeq[key].value = _cachedSeq[key].value + 1
             _cachedSeq[key].dirty = true
-            return _cachedSeq[key].sequence
+            return _cachedSeq[key].value
         else
             _cachedSeq[key] = {
                 id = key,
-                sequence = 1,
+                value = 1,
                 dirty = true,
             }
             return 1
@@ -27,14 +26,9 @@ COMPONENTS.Sequence = {
         for k, v in pairs(_cachedSeq) do
             if v.dirty then
                 table.insert(queries, {
-                    query =
-                    "INSERT INTO sequence (id, sequence) VALUES(?, ?) ON DUPLICATE KEY UPDATE sequence = VALUES(sequence)",
-                    values = {
-                        k,
-                        v.sequence,
-                    },
+                    query = "INSERT INTO `mfw_sequence` (`id`, `value`) VALUES(?, ?) ON DUPLICATE KEY UPDATE `value` = VALUES(`value`)",
+                    values = { k, v.value },
                 })
-
                 v.dirty = false
             end
         end
@@ -49,21 +43,21 @@ AddEventHandler("Core:Server:StartupReady", function()
     MySQL.rawExecute.await([[
         CREATE TABLE IF NOT EXISTS `mfw_sequence` (
             `id` VARCHAR(64) NOT NULL COLLATE 'utf8mb4_unicode_520_ci',
-            `mfw_sequence` BIGINT(20) NOT NULL DEFAULT '1',
+            `value` BIGINT(20) NOT NULL DEFAULT '1',
             PRIMARY KEY (`id`) USING BTREE
         )
         COLLATE='utf8mb4_unicode_520_ci'
         ENGINE=InnoDB
     ]])
 
-    COMPONENTS.Logger:Info("Sequence", "Ensured sequence table exists")
+    COMPONENTS.Logger:Info("Sequence", "Ensured mfw_sequence table exists")
 
-    local t = MySQL.rawExecute.await("SELECT id, sequence FROM mfw_sequence")
+    local t = MySQL.rawExecute.await("SELECT `id`, `value` FROM `mfw_sequence`")
     if t then
         for k, v in ipairs(t) do
             _cachedSeq[v.id] = {
                 id = v.id,
-                sequence = v.sequence,
+                value = v.value,
                 dirty = false,
             }
         end
